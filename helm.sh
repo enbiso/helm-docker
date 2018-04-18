33ba215c4dd82378fc60395eea16a0af580d870f
Faraj Farook <farajfarook@gmail.com>
2018-04-18 16:02:51 +1000

    adding settings

--- helm.sh
+++ helm.sh
@@ -2,7 +2,6 @@
 while [[ $# -gt 0 ]]
 do
 key="$1"
-
 case $key in
     -n|--name)
     DEPLOY_NAME="$2"
@@ -14,13 +13,23 @@ case $key in
     shift # past argument
     shift # past value
     ;;
+    -h|--hosts)
+    DEPLOY_SETTINGS="$DEPLOY_SETTINGS --set ingress.hosts={$2}"
+    shift # past argument
+    shift # past value
+    ;;
+    --set)
+    DEPLOY_SETTINGS="$DEPLOY_SETTINGS --set $2"
+    shift # past argument
+    shift # past value
+    ;;
 esac
 done
 deploys=$(helm ls | grep $DEPLOY_NAME | wc -l)
 if [ ${deploys} -eq 0 ]; then    
-    echo "Installing ${DEPLOY_NAME} on ${DEPLOY_NAMESPACE}"
-    helm install --name=${DEPLOY_NAME} . --namespace=${DEPLOY_NAMESPACE}; 
+    echo "Installing ${DEPLOY_NAME} on ${DEPLOY_NAMESPACE} with ${DEPLOY_SETTINGS}"
+    helm install --name=${DEPLOY_NAME} . --namespace=${DEPLOY_NAMESPACE} ${DEPLOY_SETTINGS}
 else 
-    echo "Upgrading ${DEPLOY_NAME} on ${DEPLOY_NAMESPACE}"
-    helm upgrade ${DEPLOY_NAME} . --namespace=${DEPLOY_NAMESPACE}; 
+    echo "Upgrading ${DEPLOY_NAME} on ${DEPLOY_NAMESPACE} with ${DEPLOY_SETTINGS}"
+    helm upgrade ${DEPLOY_NAME} . --namespace=${DEPLOY_NAMESPACE} ${DEPLOY_SETTINGS}
 fi
