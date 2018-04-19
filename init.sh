ba82564a6a10c5577694e52e46a628610e777823
Faraj Farook <farajfarook@gmail.com>
2018-04-19 20:32:23 +1000

    Changes to helm wrapper

--- init.sh
+++ init.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 mkdir -p /root/.kube     
 if [ ! -z ${KUBECONFIG_BASE64} ]; then 
+    echo "KUBECONFIG_BASE64 found and auto initializing..."
     echo ${KUBECONFIG_BASE64} | base64 -d > /root/.kube/config; 
     kubectl cluster-info; 
     helm init --client-only; 
+else
+    echo "KUBECONFIG_BASE64 not found for auto initialize."
 fi
