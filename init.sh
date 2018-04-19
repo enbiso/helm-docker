4d67f077e18e2d14562249b49e869f478ba79975
Faraj Farook <farajfarook@gmail.com>
2018-04-19 20:48:49 +1000

    Change init file and removing entrypoint

--- init.sh
+++ init.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-mkdir -p /root/.kube     
-if [ ! -z ${KUBECONFIG_BASE64} ]; then 
-    echo "KUBECONFIG_BASE64 found and auto initializing..."
-    echo ${KUBECONFIG_BASE64} | base64 -d > /root/.kube/config; 
-    kubectl cluster-info; 
-    helm init --client-only; 
-else
-    echo "KUBECONFIG_BASE64 not found for auto initialize."
+if [ ! -z $1 ]; then
+    KUBECONFIG_BASE64=$1
 fi
+if [ -z $KUBECONFIG_BASE64 ]; then
+    echo "Usage: init.sh <BASE64 encoded Kube Config file> (or set the env variable KUBECONFIG_BASE64)"
+    exit 2
+fi
+mkdir -p /root/.kube     
+echo "Initializing Kubecli and Helm"
+echo $KUBECONFIG_BASE64 | base64 -d > /root/.kube/config; 
+kubectl cluster-info; 
+helm init --client-only; 
